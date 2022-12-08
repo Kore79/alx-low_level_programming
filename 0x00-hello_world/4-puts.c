553b80753d1ecab352617997a3c815fbe5e262cc
kore79 <oorekuku@gmail.com>
2022-12-08 01:10:47 -0800

    Display String

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - display the sentence "Programming is like building a multilingual puzzle
+ * main - display the phrase "Programming is like building a multilingual puzzle
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
