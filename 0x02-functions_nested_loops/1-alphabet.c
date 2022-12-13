747578f8f14e79eb1a1df19bcb4c240481be3d46
kore79 <oorekuku@gmail.com>
2022-12-13 09:10:20 -0800

    display _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - print alphabet in lowercase
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+
+	_putchar('\n');
+
+	
+}
