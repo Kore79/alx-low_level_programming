68a89388e877e70d9c71331d7d20f2ea4d4e5561
kore79 <oorekuku@gmail.com>
2022-12-15 05:12:57 -0800

    Display 0 through 9 with _putchar

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,15 @@
 
 /**
  * print_numbers - print 0 through 9
+ * Return: void
  */
 void print_numbers(void)
 {
 	int number;
 
-	for (number = 0; number <= 9; number++)
+	for (number = 0; number < 10; number++)
 	{
-		_putchar(number + '0');
+		_putchar(number);
 	}
 
 	_putchar('\n');
