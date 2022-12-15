5929ae8d833f1ff56ceecac7766a20d6577c240c
kore79 <oorekuku@gmail.com>
2022-12-15 06:34:34 -0800

    Display 0 through 9 excluding 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,6 @@
 
 /**
  * print_most_numbers - print 0 through 9 excluding 2 and 4
- * Return: void
  */
 void print_most_numbers(void)
 {
@@ -13,5 +12,6 @@ void print_most_numbers(void)
 		if (!(number == 2 || number == 4))
 			_putchar(number);
 	}
+
 	_putchar('\n');
 }
