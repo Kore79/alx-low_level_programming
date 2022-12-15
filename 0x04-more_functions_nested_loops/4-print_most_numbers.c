fcb73dec8343169cc6f3a481dc1910e404c1610f
kore79 <oorekuku@gmail.com>
2022-12-15 06:03:26 -0800

    Display 0 through 9 excluding 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print 0 through 9 excluding 2 and 4
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	char number;
+
+	for (number = 0; number <= 9; number++)
+	{
+		if (!(number == 2 || number == 4))
+			_putchar(number);
+	}
+
+	_putchar('\n');
+}
