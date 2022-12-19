c72c1bb69647d85b8866cfe46701241ed178548a
kore79 <oorekuku@gmail.com>
2022-12-19 15:53:10 -0800

    Copy string to src including the null byte

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy the string pointed to by src to dest
+ * @dest: use char
+ * @src: use char
+ * Return: 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int s;
+
+	for (s = 0; src[a] != '\0'; s++)
+		dest[s] = src[s];
+	dest[s] = '\0';
+	return (dest);
+}
