506f112498ad192bdc527950767d7038444ceab0
kore79 <oorekuku@gmail.com>
2022-12-19 15:22:30 -0800

    display half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - print half of a string
+ * @str: use char to check
+ * Return: 0
+ */
+void puts_half(char *str)
+{
+	int h_string = 0, i;
+
+	while (str[h_string] != '\0')
+		h_string++;
+	if (h_string + 1 % 2 != '0')
+		i = (h_string - 1) / 2;
+	else
+		i = (h_string / 2);
+	i++
+
+		for (h_string = i; str[h_string] != '\0'; h_string++)
+			_putchar(str[h_string]);
+	_putchar('\n');
+}
