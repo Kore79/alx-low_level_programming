8ca6aef17f62b571acfb1018187f12f07eaeebdc
kore79 <oorekuku@gmail.com>
2022-12-21 12:53:55 -0800

    Print integer with a function

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - prints integer with a function
+ * @n: number to print
+ */
+void print_number(int n)
+{
+	unsigned int num;
+
+	if (n < 0)
+	{
+		_putchar('_');
+		n *= -1;
+	}
+
+	num = n;
+
+	if (num / 10)
+		print_number(num / 10);
+
+	_putchar(num % 10 + '0');
+}
